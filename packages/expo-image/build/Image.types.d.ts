8abcc9aff39b945628019078e19689c37ccb749c
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-16 12:36:22 +0100

    [expo-image] Add types for events

---
+++ packages/expo-image/build/Image.types.d.ts
@@ -0,0 +1,22 @@
+export declare enum ImageCacheType {
+    LOCAL_OR_UNKNOWN = 0,
+    REMOTE = 1,
+    DISK = 2,
+    MEMORY = 3
+}
+export interface ImageLoadEventData {
+    cacheType?: ImageCacheType;
+    source: {
+        url: string;
+        width: number;
+        height: number;
+        mediaType?: string;
+    };
+}
+export interface ImageLoadProgressEventData {
+    loaded: number;
+    total: number;
+}
+export interface ImageErrorEventData {
+    error: string;
+}
---
+++ packages/expo-image/build/Image.types.js
@@ -0,0 +1,8 @@
+export var ImageCacheType;
+(function (ImageCacheType) {
+    ImageCacheType[ImageCacheType["LOCAL_OR_UNKNOWN"] = 0] = "LOCAL_OR_UNKNOWN";
+    ImageCacheType[ImageCacheType["REMOTE"] = 1] = "REMOTE";
+    ImageCacheType[ImageCacheType["DISK"] = 2] = "DISK";
+    ImageCacheType[ImageCacheType["MEMORY"] = 3] = "MEMORY";
+})(ImageCacheType || (ImageCacheType = {}));
+//# sourceMappingURL=Image.types.js.map
